7ba848da16cf0c50403dcb7e932165ffd55d9531
mink0 <mlazer@gmail.com>
2018-08-04 14:22:23 +0300

    Update godoc

--- doc.go
+++ doc.go
@@ -4,17 +4,12 @@
 Package execmd is a Golang library providing a simple interface to shell commands execution
 
 Features
-	- execute command in system shell, so you could use variables, pipes, redirections
-
-	- execute local and remote shell commands
-
-	- interface is similar to [exec](https://golang.org/pkg/os/exec/)
-
-	- realtime `stdout` and `stderr` output with fancy colors and prefixes
-
-	- remote commands execution is implemented by wrapping standart [OpenSSH](https://www.openssh.com/) SSH client,
-		so all your ssh configuration (including ssh agent forwarding) works as expected
-
-	- run remote commands on several hosts (parallel and serial execution supported)
+		* execute commands in system shell, so you could use variables, pipes, redirections
+		* execute local and remote shell commands
+		* interface is similar to [exec](https://golang.org/pkg/os/exec/)
+		* realtime `stdout` and `stderr` output with fancy colors and prefixes
+		* remote commands execution is implemented by wrapping standart [OpenSSH](https://www.openssh.com/) SSH client,
+			so all your ssh configuration (including ssh agent forwarding) works as expected
+		* run remote commands on several hosts (parallel and serial execution supported)
 */
 package execmd
