6064c629f3aa04cb5694f35b3de2a83d8215b829
mink0 <mlazer.devel@gmail.com>
2023-04-13 12:56:58 +0300

    Improve readability

--- cmd.go
+++ cmd.go
@@ -1,11 +1,6 @@
-/*
-	Wrapper for https://golang.org/pkg/os/exec/ to invoke command in shell,
-	pipe stdout, stderr to console with prefixes,
-	record output buffers
-
-	Copyright(c) 2018 mink0
-*/
-
+// Package execmd provides a wrapper around https://golang.org/pkg/os/exec/
+// to execute commands in a shell, pipe stdout and stderr to the console with
+// prefixes, and record output buffers.
 package execmd
 
 import (
@@ -16,13 +11,12 @@ import (
 	"os/exec"
 )
 
-// Path to system shell binary
-// Could be overridden by setting `SHELL` env variable
+// Default shell paths that can be overridden by setting the `SHELL` environment variable.
 var shellPathList = []string{os.Getenv("SHELL"), "bash", "sh"}
 
-// Cmd is wrapper struct around exec.Cmd
-// Stream buffers will be saved with Record[Stdout|Stderr] == true
-// Mute options turns off console output
+// Cmd is a wrapper struct around exec.Cmd that provides additional
+// functionality such as recording and muting stdout and stderr, and
+// customizing output prefixes.
 type Cmd struct {
 	ShellPath    string
 	Interactive  bool
@@ -32,20 +26,20 @@ type Cmd struct {
 	MuteStdout   bool
 	MuteStderr   bool
 	MuteCmd      bool
-	PrefixStdout string // printing prefixes
-	PrefixStderr string //
-	PrefixCmd    string //
+	PrefixStdout string
+	PrefixStderr string
+	PrefixCmd    string
 
-	Cmd *exec.Cmd // os.Exec instance
+	Cmd *exec.Cmd
 }
 
-// CmdRes resulting struct
+// CmdRes represents the result of a command, including the stdout and stderr buffers.
 type CmdRes struct {
 	Stdout *bytes.Buffer
 	Stderr *bytes.Buffer
 }
 
-// NewCmd initializes Cmd with defaults
+// NewCmd initializes a Cmd with default settings.
 func NewCmd() *Cmd {
 	cmd := Cmd{
 		RecordStdout: true,
@@ -55,7 +49,6 @@ func NewCmd() *Cmd {
 		PrefixStderr: colorErr("@err "),
 	}
 
-	// try to find shell binary
 	if shellPath, err := findPath(shellPathList); err == nil {
 		cmd.ShellPath = shellPath
 	}
@@ -63,28 +56,28 @@ func NewCmd() *Cmd {
 	return &cmd
 }
 
-// Wait is a exec.Wait wrapper with buffer flushes
-func (c *Cmd) Wait() (err error) {
-	err = c.Cmd.Wait()
+// Wait wraps exec.Wait and ensures that the buffers are flushed after waiting.
+func (c *Cmd) Wait() error {
+	err := c.Cmd.Wait()
 
-	// flush last line
 	c.Cmd.Stderr.(*pStream).Close()
 	c.Cmd.Stdout.(*pStream).Close()
-	return
+	return err
 }
 
-// Run is exec.Run() wrapper: runs command and blocking wait for result
-func (c *Cmd) Run(command string) (res CmdRes, err error) {
-	if res, err = c.Start(command); err != nil {
-		return
+// Run executes the command and waits for the result.
+func (c *Cmd) Run(command string) (CmdRes, error) {
+	res, err := c.Start(command)
+	if err != nil {
+		return res, err
 	}
 
 	err = c.Wait()
-	return
+	return res, err
 }
 
-// Start is exec.Start() wrapper with system shell and output buffers initialization
-func (c *Cmd) Start(command string) (res CmdRes, err error) {
+// Start initializes the system shell and output buffers, and starts the command.
+func (c *Cmd) Start(command string) (CmdRes, error) {
 	args := []string{}
 	if c.Interactive {
 		args = append(args, "-i")
@@ -98,7 +91,6 @@ func (c *Cmd) Start(command string) (res CmdRes, err error) {
 
 	c.Cmd = exec.Command(c.ShellPath, args...)
 
-	// FIXME: rewrite to use raw buffers only when mute == true
 	stdoutLogFile := log.New(os.Stdout, "", 0)
 	if c.MuteStdout {
 		stdoutLogFile = log.New(bytes.NewBuffer([]byte("")), "", 0)
@@ -114,7 +106,6 @@ func (c *Cmd) Start(command string) (res CmdRes, err error) {
 
 	stderrStream := newPStream(stderrLogFile, c.PrefixStderr, c.RecordStderr)
 	c.Cmd.Stderr = stderrStream
-
 	if c.Interactive {
 		c.Cmd.Stdin = os.Stdin
 	}
@@ -123,20 +114,23 @@ func (c *Cmd) Start(command string) (res CmdRes, err error) {
 		fmt.Printf("%s%s\n", c.PrefixCmd, colorStrong(command))
 	}
 
-	err = c.Cmd.Start()
+	err := c.Cmd.Start()
 
-	res.Stdout = stdoutStream.Get()
-	res.Stderr = stderrStream.Get()
-	return
+	res := CmdRes{
+		Stdout: stdoutStream.Get(),
+		Stderr: stderrStream.Get(),
+	}
+	return res, err
 }
 
-func findPath(paths []string) (path string, err error) {
+// findPath searches for the available shell path from a given list of paths.
+func findPath(paths []string) (string, error) {
 	for _, p := range paths {
-		path, err = exec.LookPath(p)
+		path, err := exec.LookPath(p)
 		if err == nil {
-			break
+			return path, nil
 		}
 	}
 
-	return
+	return "", fmt.Errorf("no valid shell found in path list")
 }
